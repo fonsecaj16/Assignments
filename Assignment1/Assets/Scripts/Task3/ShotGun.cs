4b2360fab6c6d5cc5962571fab5f43788dccb7ee
fonsecaj16 <jfonseca193@gmail.com>
2023-12-20 19:50:47 -0500

    Small fixes

--- Assignment1/Assets/Scripts/Task3/ShotGun.cs
+++ Assignment1/Assets/Scripts/Task3/ShotGun.cs
@@ -1,13 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum Direction
-{
-    up = 0,
-    left = 1,
-    down = 2,
-    right = 3
-}
 public class ShotGun : ReloadableWeaponBase
 {
     // Start is called before the first frame update
