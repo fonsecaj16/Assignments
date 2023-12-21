7b8624092b2d3b1caa181c588245953513f54f83
fonsecaj16 <jfonseca193@gmail.com>
2023-12-20 19:50:01 -0500

    Small fixes

--- Assignment1/Assets/Scripts/Task3/ShotGun.cs
+++ Assignment1/Assets/Scripts/Task3/ShotGun.cs
@@ -1,7 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum Direction
+{
+    up = 0,
+    left = 1,
+    down = 2,
+    right = 3
+}
 public class ShotGun : ReloadableWeaponBase
 {
     // Start is called before the first frame update
@@ -17,22 +23,32 @@ public class ShotGun : ReloadableWeaponBase
     {
         Debug.Log("Shooting Shotgun");
         wasteAmmo(3);
-        Vector3 shootDirection = Quaternion.Euler(45, 0, 0) * transform.forward;
-        Vector3 shootDirection2 = Quaternion.Euler(-45, 0, 0) * transform.forward;
-        Vector3 shootDirection3 = Quaternion.Euler(0, 45, 0) * transform.forward;
-        Vector4 shootDirection4 = Quaternion.Euler(0, -45, 0) * transform.forward;
-        GameObject bullet1 = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); 
-        GameObject bullet2 = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        GameObject bullet3 = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        GameObject bullet4 = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        bullet1.GetComponent<Rigidbody>().AddForce(shootDirection * projectileSpeed);
-        bullet2.GetComponent<Rigidbody>().AddForce(shootDirection2 * projectileSpeed);
-        bullet3.GetComponent<Rigidbody>().AddForce(shootDirection3* projectileSpeed);
-        bullet4.GetComponent<Rigidbody>().AddForce(shootDirection4 * projectileSpeed);
-        Destroy(bullet1, 1);
-        Destroy(bullet2, 1);
-        Destroy(bullet3, 1);
-        Destroy(bullet4, 1);
-        
+        spawnBullet(0);
+        spawnBullet(1);
+        spawnBullet(2);
+        spawnBullet(3);        
+    }
+    private void spawnBullet(int direction)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Vector3 shootDirection = Vector3.forward;
+        switch (direction)
+        {
+            case 0:
+                shootDirection = Quaternion.Euler(45, 0, 0) * transform.forward;
+                break;
+            case 1:
+                shootDirection = Quaternion.Euler(-45, 0, 0) * transform.forward;
+                break;
+            case 2:
+                shootDirection = Quaternion.Euler(0, 45, 0) * transform.forward;
+                break;
+            case 3:
+                shootDirection = Quaternion.Euler(0, -45, 0) * transform.forward;
+                break;
+
+        }
+        bullet.GetComponent<Rigidbody>().AddForce(shootDirection * projectileSpeed);
+        Destroy(bullet, 1);
     }
 }
